1d7939192d1dc1745d0cc606ba516b49fc9e5a23
Vladislav Sevostyanov <vladislav.sevostyanov@kidl.no>
2019-06-12 10:17:11 +0300

    redoc, правки

--- swagger.js
+++ swagger.js
@@ -20,6 +20,91 @@ module.exports = {
       in: 'header',
     },
   },
+  definitions: {
+    schemas: {
+      NeedlesItem: {
+        type: 'object',
+        properties: {
+          sku: {
+            type: 'string',
+            example: '100-35911',
+          },
+          enabled: {
+            type: 'boolean',
+            example: true,
+          },
+          name: {
+            type: 'string',
+            example: 'Basix Birch, 60 cm, 15.0 mm - Rundpinner i bjørk',
+          },
+          thickness: {
+            type: 'string',
+            example: '15',
+          },
+          length: {
+            type: 'string',
+            example: '60',
+          },
+          price: {
+            type: 'number',
+            example: 91,
+          },
+          price_merchant: {
+            type: 'number',
+            example: 91,
+          },
+          image: {
+            type: 'object',
+            properties: {
+              swatch: {
+                type: 'string',
+                example: 'https://stage-hofy.kidl.no/media/catalog/product/1/0/100-35911_kp_basix_bj_rk_rundpinne_60cm__15mm.jpg',
+              },
+              base: {
+                type: 'string',
+                example: 'https://stage-hofy.kidl.no/media/catalog/product/1/0/100-35911_kp_basix_bj_rk_rundpinne_60cm__15mm.jpg',
+              },
+            },
+          },
+        },
+      },
+      Needles: {
+        type: 'object',
+        properties: {
+          sku: {
+            type: 'string',
+            example: 'Basix Birch - Rundpinner',
+          },
+          enabled: {
+            type: 'boolean',
+            example: true,
+          },
+          name: {
+            type: 'string',
+            example: 'Rundpinner',
+          },
+          brand: {
+            type: 'string',
+            example: 'Basix Birch',
+          },
+          material: {
+            type: 'string',
+            example: 'Bjørk',
+          },
+          color: {
+            type: 'string',
+            example: 'Natur',
+          },
+          items: {
+            type: 'array',
+            items: {
+              $ref: '#/definitions/schemas/NeedlesItem',
+            },
+          },
+        },
+      },
+    }
+  },
   externalDocs: {
     description: 'Find out more about service',
     url: 'https://kidlno.atlassian.net/wiki/spaces/HY/pages/24838147/Microservices+Woolit',
